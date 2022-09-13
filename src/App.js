8e4e487434628dc54e01bd086129fcfa80189e39
mayankkh1 <42695637+mayankkh1@users.noreply.github.com>
2022-09-13 16:51:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <h2>This is a my triggered build on jenkins myapp</h2>
+      <h2>This is a my triggered build on jenkins myapp2</h2>
     </div>
   );
 }
